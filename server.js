e69a182b4a5b776b35209dfc33e9bf2c06b3e1d2
Grace Hartzell <gracehartzell@utexas.edu>
2018-09-18 21:06:10 -0500

    Added routes and pages to connect tomorrow

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const knex = require('./db/knex');
+const favicon = require('serve-favicon');
+const path = require('path');
+const methodOverride = require('method-override');
 const port = process.env.PORT || 3000;
 
+const knexPath = path.join(__dirname, 'knexfile.js');
+const knex = require('./db/knex');
+
+// const assassins = require('./routes/assassins');
+// const contracts = require('./routes/contracts');
+
 const app = express();
+app.use(favicon(path.join(__dirname, 'public', 'gun_favicon.ico')));
+app.use(express.static(path.join('public')));
 app.disable('x-powered-by');
 app.set('view engine','ejs');
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(assassins);
+// app.use(contracts);
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/assassins', (req, res) => {
+    res.render('../assassins');
+});
 
+app.use((req, res) => {
+    res.sendStatus(404);
+})
 
 app.listen(port, () => {
     console.log(`Listening on PORT ${port}`);
